dc7e40e83faba18740e161e07e50c53964b65832
Miguel Najera <migueln1987@gmail.com>
2017-02-11 17:47:46 -0600

    Add entry to .gitignore, Add test function to HelloWorld.scala, Add tests for test function in HelloWorldSpec.scala

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,5 +1,10 @@
 object HelloWorld {
+  def addTwo(x: Int, y: Int): Int = {
+    x + y
+  }
+
   def main(args: Array[String]): Unit = {
     println("Hello World")
+    println(addTwo(3, 4))
   }
 }
