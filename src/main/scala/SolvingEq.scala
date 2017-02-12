b7effe2e05d73493c848654b0b292b31a798be6e
Miguel Najera <migueln1987@gmail.com>
2017-02-11 23:08:03 -0600

    Change f(x) to one with roots, implement IVT function, change results for f(x) tests added test for IVT

--- src/main/scala/SolvingEq.scala
+++ src/main/scala/SolvingEq.scala
@@ -1,14 +1,18 @@
 object SolvingEq {
-  def f(x: BigDecimal): BigDecimal = x.pow(2) + x + 3
+  def f(x: Double): Double = scala.math.pow(x, 2) + x - 3
+  def g(x: Double): Double = ???
+  def h(x: Double): Double = ???
 
-  def IVT(f: BigDecimal => BigDecimal, x: BigDecimal): Double = {
-    return 0.0
+  def IVT(f: Double => Double, x: Double): (Double, Double) = {
+    if (f(x) * f (x + 1) < 0) return (x, x + 1)
+    else IVT(f, x + 1)
   }
 
   def main(args: Array[String]): Unit = {
-    println("f(x) = x^2 + x + 3")
+    println("f(x) = x^2 + x - 3")
     println("f(0) = " + f(0))
     println("f(1) = " + f(1))
     println("f(2) = " + f(2))
+    println(IVT(f, 0))
   }
 }
--- src/test/scala/SolvingEqSpec.scala
+++ src/test/scala/SolvingEqSpec.scala
@@ -1,13 +1,16 @@
 import org.scalatest.FunSuite
 
 class SolvingEqSpec extends FunSuite {
-  test("f(x) = x^2 + x + 3 with x = 0 should return 3.0") {
-    assert(SolvingEq.f(0.0) == 3.0)
+  test("f(x) = x^2 + x + 3 with x = 0 should return -3.0") {
+    assert(SolvingEq.f(0.0) == -3.0)
   }
-  test("f(x) = x^2 + x + 3 with x = 1 should return 5.0") {
-    assert(SolvingEq.f(1.0) == 5.0)
+  test("f(x) = x^2 + x + 3 with x = 1 should return -1.0") {
+    assert(SolvingEq.f(1.0) == -1.0)
   }
-  test("f(x) = x^2 + x + 3 with x = 2 should return 9.0") {
-    assert(SolvingEq.f(2.0) == 9.0)
+  test("f(x) = x^2 + x + 3 with x = 2 should return 3.0") {
+    assert(SolvingEq.f(2.0) == 3.0)
+  }
+  test("Interval with a root of f(x) = x^2 + x - 3 should return (1.0, 2.0)") {
+    assert(SolvingEq.IVT(SolvingEq.f, 0) == (1.0, 2.0))
   }
 }
